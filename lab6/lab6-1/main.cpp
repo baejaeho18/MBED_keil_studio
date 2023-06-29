ac03d385c0663de9faf9d0057c679581945cde1a
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-06-29 16:29:15 +0900

    Update main.cpp

--- lab6/lab6-1/main.cpp
+++ lab6/lab6-1/main.cpp
@@ -1,34 +1,31 @@
 #include "mbed.h"
 
+DigitalOut led2(PB_3) ;
+InterruptIn button(BUTTON1) ; 
+Ticker led2_ticker ;
 
-DigitalOut led2(PB_3);
-InterruptIn button(BUTTON1);
-
-Ticker led2_ticker;
-
-float interval = 0.5;
+float interval = 0.5 ;
 
 void timeout_cb(void)
 {
-    led2=!led2;
+    led2 =! led2 ;
 }
 
 void button_onpressed_cb(void)
 {
-    led2_ticker.detach();
-    interval=interval*2;
-    if(interval>4.0){
-        interval = 0.125;
-    }
-    led2=1;
-    led2_ticker.attach(&timeout_cb,interval);
-
+    led2_ticker.detach() ;
+    interval = interval * 2 ;
+    if (interval > 4.0) 
+            interval = 0.125 ;
+    led2 = 1 ;
+    led2_ticker.attach(&timeout_cb, interval) ;
 }
+
 int main()
 {
-    button.mode(PullUp);
-    button.fall(&button_onpressed_cb);
+    button.mode(PullUp) ;
+    button.fall(&button_onpressed_cb) ;
 
-    led2_ticker.attach(&timeout_cb,interval);
+    led2_ticker.attach(&timeout_cb, interval) ;
     while(1){}
 }
