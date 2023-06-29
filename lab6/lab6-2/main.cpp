96857572126539aa25b25b359b37a849c656b2c6
baejaeho18 <bjh1749@gmail.com>
2023-06-29 16:26:51 +0900

    lab6

---
+++ lab6/lab6-2/main.cpp
@@ -0,0 +1,30 @@
+#include "mbed.h"
+
+
+DigitalOut led2(PB_3);
+DigitalOut led3(PB_5);
+
+
+Ticker led2_ticker;
+Ticker led3_ticker;
+
+float interval = 1;
+
+void timeout_cb(void)
+{
+    led2_ticker.detach();
+    led2=!led2;
+    led2_ticker.attach(&timeout_cb,interval);
+}
+void timeout_cb2(void)
+{
+    led3=!led3;
+}
+
+int main()
+{
+    led3=1;
+    led2_ticker.attach(&timeout_cb,interval);
+    led3_ticker.attach(&timeout_cb2,interval*10);
+    while(1){}
+}
---
+++ lab6/lab6-3/main.cpp
@@ -0,0 +1,21 @@
+#include "mbed.h"
+#include <stdio.h>
+
+Timer t;
+#define MAXIMUM_BUFFER_SIZE 80
+char buf[MAXIMUM_BUFFER_SIZE];
+
+static BufferedSerial pc(CONSOLE_TX, CONSOLE_RX, 115200);
+
+int main()
+{   
+
+    int i=0;
+    t.start();
+    while(i<1000000){
+        i+=1;
+    }
+    t.stop();
+    sprintf(buf,"the time taked was %llu us\r\n",(t.elapsed_time()).count());
+    pc.write(buf, strlen(buf));
+}
