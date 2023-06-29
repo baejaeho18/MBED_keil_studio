aa200a01fa7897ce58b0a9c64689ddbdeb016085
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-06-29 14:22:40 +0900

    Update main.cpp

--- lab1-2 InterruptIn button/main.cpp
+++ lab1-2 InterruptIn button/main.cpp
@@ -5,31 +5,29 @@
 
 #include "mbed.h"
 
-DigitalOut greenLED(LED1);
-DigitalOut redLED(D2);
-InterruptIn button (BUTTON1);
+DigitalOut greenLED(LED1) ;
+DigitalOut redLED(D2) ;
+InterruptIn button (BUTTON1) ;
 
+volatile int interval = 500 ;
 
-volatile int interval=500;
-
-void buttonPressed()
+void 
+buttonPressed()
 {
-    redLED = !redLED;
-    if(interval<4000){
-        interval += 500;
-    }
+    redLED = !redLED ;
+    if (interval < 4000)
+        interval += 500 ;
     else
-        interval = 500;
+        interval = 500 ;
 }
 
-int main()
+int 
+main()
 {
-    button.fall(&buttonPressed);
+    button.fall(&buttonPressed) ;
 
     while (true) {
-       
-       greenLED = !greenLED;
-       ThisThread::sleep_for(std::chrono::milliseconds(interval));
-
+       greenLED = !greenLED ;
+       ThisThread::sleep_for(std::chrono::milliseconds(interval)) ;
     }
 }
