719feb15baa93132f085846bfb0095ae2df3d406
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-06-29 14:21:05 +0900

    Update main.cpp

--- lab1-1 DigitalIn button/main.cpp
+++ lab1-1 DigitalIn button/main.cpp
@@ -1,28 +1,22 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
-DigitalOut led(LED1);
-DigitalIn button (BUTTON1);
+DigitalOut led(LED1) ;
+DigitalIn button (BUTTON1) ;
 
 
 int main()
 {
-    int interval = 500;
+    int interval = 500 ;
 
     while (true) {
        
-       if (button ==0){
-           interval = interval<<1;
-           if(interval>2000)
-            interval = 250;
+       if (button == 0) {
+           interval = interval << 1 ;
+           if (interval > 2000)
+            interval = 250 ;
        }
-       led = !led;
-       ThisThread::sleep_for(
-           std::chrono::milliseconds(interval));
-
+       led = !led ;
+       ThisThread::sleep_for (
+           std::chrono::milliseconds(interval)) ;
     }
 }
