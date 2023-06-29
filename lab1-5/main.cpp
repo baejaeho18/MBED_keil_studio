32f4c05fabc20c55f49441f32ff2aaf0538c2b24
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-06-29 14:28:42 +0900

    Update main.cpp

--- lab1-5/main.cpp
+++ lab1-5/main.cpp
@@ -1,30 +1,25 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
-//D3:PB_3, D4:PB_5, D5:PB_4
-//D3:red, D5:green, D4:yellow
-PortOut RGYLeds(PortB, 0x0038);
+// D3:PB_3, D4:PB_5, D5:PB_4
+// D3:red, D5:green, D4:yellow
+PortOut RGYLeds(PortB, 0x0038) ;
 
 int main()
 {
-    while (true) {
-       int counter = 0;
-       for(int color = 0; color<8; color++){
-           if (color==2||color==3){
-               counter=2;
-           }
-           else if (color==4||color==5){
-               counter=(-2);
-           }
+    while (true)
+    {
+       int counter = 0 ;
+       for (int color = 0 ; color < 8 ; color++)
+       {
+           if (color == 2 || color == 3)
+               counter = 2 ;
+           else if (color == 4 || color == 5)
+               counter = -2 ;
            else
-                counter=0;
-           RGYLeds = (color+counter)<<3;
-           ThisThread::sleep_for(500ms);
+                counter = 0 ;
+           
+           RGYLeds = (color + counter) << 3 ;
+           ThisThread::sleep_for(500ms) ;
        }
-
     }
 }
