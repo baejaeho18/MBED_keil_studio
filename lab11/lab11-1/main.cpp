c45de14ea92835a7ecc1ecb7fc838b3f9ec4cbb1
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-01 15:57:46 +0900

    Update main.cpp

--- lab11/lab11-1/main.cpp
+++ lab11/lab11-1/main.cpp
@@ -52,32 +52,33 @@ int main()
         }
         else
         {
-                uint16_t raw_temp = ((uint16_t)data[0] << 8) | (uint16_t)data[1];
-                raw_temp &= 0xFFFC;
-                float temp = -46.85 + 175.72 * ((float)raw_temp / 65536.0);
-                sprintf(buffer, "Temperature = %.2f [C]\r\n", temp);
-                pc.write(buffer, strlen(buffer));
+                uint16_t raw_temp = ((uint16_t)data[0] << 8) | (uint16_t)data[1] ;
+                raw_temp &= 0xFFFC ;
+                float temp = -46.85 + 175.72 * ((float)raw_temp / 65536.0) ;
+                sprintf(buffer, "Temperature = %.2f [C]\r\n", temp) ;
+                pc.write(buffer, strlen(buffer)) ;
         }
 
-        cmd[0] = HTU21D_HUMI_READ;
-        i2c.write(HTU21D_ADDR << 1, cmd, 1, true);
-        ThisThread::sleep_for(50ms); // wait for measurement
-        i2c.read(HTU21D_ADDR << 1, data, 2, true);
-        if (!button) { 
-            data[0] += 1;  
+        cmd[0] = HTU21D_HUMI_READ ;
+        i2c.write(HTU21D_ADDR << 1, cmd, 1, true) ;
+        ThisThread::sleep_for(50ms) ; // wait for measurement
+        i2c.read(HTU21D_ADDR << 1, data, 2, true) ;
+        if (!button)
+            data[0] += 1 ;  
+        if (calculate_crc((uint8_t*)data, 2) != data[2])
+        {
+            sprintf(buffer, "Humidity CRC Error\r\n") ;
+            pc.write(buffer, strlen(buffer)) ;
         }
-        if (calculate_crc((uint8_t*)data, 2) != data[2]) {
-            sprintf(buffer, "Humidity CRC Error\r\n");
-            pc.write(buffer, strlen(buffer));
-        }
-        else {
-                uint16_t raw_humi = ((uint16_t)data[0] << 8) | (uint16_t)data[1];
+        else
+        {
+                uint16_t raw_humi = ((uint16_t)data[0] << 8) | (uint16_t)data[1] ;
                 raw_humi &= 0xFFF0 ;
-                float humi = -6.0 + 125.0 * ((float)raw_humi / 65536.0);
-                sprintf(buffer, "Relative Humidity = %.2f [%%]\r\n",humi);
-                pc.write(buffer, strlen(buffer));
+                float humi = -6.0 + 125.0 * ((float)raw_humi / 65536.0) ;
+                sprintf(buffer, "Relative Humidity = %.2f [%%]\r\n",humi) ;
+                pc.write(buffer, strlen(buffer)) ;
         }
 
-        ThisThread::sleep_for(3000ms);
+        ThisThread::sleep_for(3000ms) ;
     }
 }
