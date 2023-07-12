c0dd04411dc16651a00f82a3de099295fafe9311
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-12 18:49:15 +0900

    Update main.cpp

--- lab9/lab9-2/main.cpp
+++ lab9/lab9-2/main.cpp
@@ -1,12 +1,13 @@
 #include "mbed.h"
 #include "SRF05.h"
 
-SRF05 srf05(ARDUINO_UNO_D3,ARDUINO_UNO_D2);
+SRF05 srf05(ARDUINO_UNO_D3,ARDUINO_UNO_D2) ;
 
-BufferedSerial pc(CONSOLE_TX,CONSOLE_RX, 115200);
-char buffer[80];
+BufferedSerial pc(CONSOLE_TX,CONSOLE_RX, 115200) ;
+char buffer[80] ;
 
-int main(){
+int main()
+{
 
     sprintf(buffer,"Mbed OS version %d.%d%d\r\n\n",
             MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
