b43e218c3e77e2abc13c4ba61dd0b0847ec26859
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-13 16:50:32 +0900

    Update IntDebouncedIn.cpp

--- midterm/midterm/IntDebouncedIn.cpp
+++ midterm/midterm/IntDebouncedIn.cpp
@@ -1,73 +1,73 @@
 #include "IntDebouncedIn.h"
 
-IntDebounceIn::IntDebounceIn(PinName pin, std::chrono::milliseconds debounceTime) :
+IntDebounceIn::IntDebounceIn(PinName pin, std::chrono::milliseconds debounceTime):
     interrupt(pin),
-    state(Released)  //0 (시작state)
+    state(Released)  // 0 (시작state)
 {
-    debounce_ticker.attach(callback(this, &IntDebounceIn::debounce), debounceTime);
+    debounce_ticker.attach(callback(this, &IntDebounceIn::debounce), debounceTime) ;
 
-    interrupt.fall(callback(this, &IntDebounceIn::fall_Interrupt));
-    interrupt.rise(callback(this, &IntDebounceIn::rise_Interrupt));
+    interrupt.fall(callback(this, &IntDebounceIn::fall_Interrupt)) ;
+    interrupt.rise(callback(this, &IntDebounceIn::rise_Interrupt)) ;
 }
 
 void IntDebounceIn::fall(Callback<void()> Callback_Func) {
-    fall_Callback =Callback_Func;
+    fall_Callback = Callback_Func ;
 }
 
 void IntDebounceIn::rise(Callback<void()> Callback_Func) {
-    rise_Callback=Callback_Func;
+    rise_Callback = Callback_Func ;
 }
 
 void IntDebounceIn::fall_Interrupt() {
-    if (state==Released) {
-        state=PrePressed1;
+    if (state == Released) {
+        state = PrePressed1 ;
     }
 }
 
 void IntDebounceIn::rise_Interrupt() {
-    if (state==Pressed) {
-        state=PreReleased1;
+    if (state == Pressed) {
+        state = PreReleased1 ;
     }
 }
 
 void IntDebounceIn::debounce() {
-    int input_value = interrupt.read();
+    int input_value = interrupt.read() ;
     switch (state) {
         case PrePressed1:
             if (!input_value) {
-                state =PrePressed2;
+                state = PrePressed2 ;
             } else {
-                state=Released;
+                state = Released ;
             }
-            break;
+            break ;
 
         case PrePressed2:
             if (!input_value) {
-                state = Pressed;
-                fall_Callback();
+                state = Pressed ;
+                fall_Callback() ;
             } else {
-                state=Released;
+                state=Released ;
             }
-            break;
+            break ;
 
         case PreReleased1:
             if (input_value) {
-                state =PreReleased2;
+                state = PreReleased2 ;
             } else {
-                state=Pressed;
+                state = Pressed ;
             }
             break;
 
         case PreReleased2:
             if (input_value) {
-                state = Released;
-                rise_Callback();
+                state = Released ;
+                rise_Callback() ;
             } else {
-                state=Pressed;
+                state = Pressed ;
             }
-            break;
+            break ;
 
         default:
-            break;
+            break ;
     }
 }
