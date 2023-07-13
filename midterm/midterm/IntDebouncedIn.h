c9cb297d50362d6cff0671dfa1c82f4be66adc8a
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-13 16:51:36 +0900

    Update IntDebouncedIn.h

--- midterm/midterm/IntDebouncedIn.h
+++ midterm/midterm/IntDebouncedIn.h
@@ -6,30 +6,30 @@
 #define DEFAULT_TIME 20ms
 
 typedef enum {
-    Released, //0
-    PrePressed1, //1
-    PrePressed2, //2
-    Pressed, //3
-    PreReleased1, //4
-    PreReleased2 //5
-} swState; //state머신을 위한 변수설정
+    Released, // 0
+    PrePressed1, // 1
+    PrePressed2, // 2
+    Pressed, // 3
+    PreReleased1, // 4
+    PreReleased2 // 5
+} swState ; // state머신을 위한 변수설정
 
 class IntDebounceIn {
     public:
-        IntDebounceIn(PinName pin, std::chrono::milliseconds debounceTime = DEFAULT_TIME);
-        void fall(Callback<void()> Callback_Func);
-        void rise(Callback<void()> Callback_Func);
+        IntDebounceIn(PinName pin, std::chrono::milliseconds debounceTime = DEFAULT_TIME) ;
+        void fall(Callback<void()> Callback_Func) ;
+        void rise(Callback<void()> Callback_Func) ;
     private:
-        InterruptIn interrupt;
-        swState state;
-        Ticker debounce_ticker;
+        InterruptIn interrupt ;
+        swState state ;
+        Ticker debounce_ticker ;
 
-        Callback<void()> fall_Callback;
-        Callback<void()> rise_Callback;
+        Callback<void()> fall_Callback ; 
+        Callback<void()> rise_Callback ;
 
-        void fall_Interrupt();
-        void rise_Interrupt();
-        void debounce();
+        void fall_Interrupt() ;
+        void rise_Interrupt() ;
+        void debounce() ;
 };
 
 #endif
