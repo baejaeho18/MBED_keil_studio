eaac73a68bcb936dacdd959020a2183989a45ef5
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-13 16:52:34 +0900

    Update main.cpp

--- midterm/midterm/main.cpp
+++ midterm/midterm/main.cpp
@@ -4,27 +4,31 @@
 
 #define BLINKING_RATE 500ms
 
-IntDebounceIn debouncedSW(D7, 20ms);
-UnbufferedSerial pc(CONSOLE_TX, CONSOLE_RX, 230400);
-DigitalOut led(LED1);
+IntDebounceIn debouncedSW(D7, 20ms) ;
+UnbufferedSerial pc(CONSOLE_TX, CONSOLE_RX, 230400) ;
+DigitalOut led(LED1) ;
 
-void isr_pushed () {
-    char ch;
-    ch = 'P';
-    pc.write(&ch, 1);
+void isr_pushed () 
+{
+    char ch ;
+    ch = 'P' ;
+    pc.write(&ch, 1) ;
 }
 
-void isr_released () {
-    char ch;
-    ch = 'R';
-    pc.write(&ch, 1);
+void isr_released () 
+{
+    char ch ;
+    ch = 'R' ;
+    pc.write(&ch, 1) ;
 }
+
 int main()
 {   
-    debouncedSW.fall(&isr_pushed);
-    debouncedSW.rise(&isr_released);
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+    debouncedSW.fall(&isr_pushed) ; 
+    debouncedSW.rise(&isr_released) ;
+    while (true) 
+    {
+        led = !led ;
+        ThisThread::sleep_for(BLINKING_RATE) ;
     }
 }
