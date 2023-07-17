8a3098973c0ac8f5272c823fd661a831b2e96adb
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-17 10:18:44 +0900

    Update I2C.cpp

--- final_project_control/master/I2C.cpp
+++ final_project_control/master/I2C.cpp
@@ -18,24 +18,24 @@ void I2C_Config (void)
 
 void I2C_Write(uint8_t data)
 {
-    while (!(I2C1->SR1 & (1<<7))) ; //wait for TXE bit to set
+    while (!(I2C1->SR1 & (1 << 7))) ; //wait for TXE bit to set
     I2C1->DR = data ;
-    while (!(I2C1->SR1 & (1<<2))) ;   //wait for BTF bit to set
+    while (!(I2C1->SR1 & (1 << 2))) ;   //wait for BTF bit to set
 }
 
 void I2C_Start (void)
 {
-    I2C1->CR1 |= (1<<10) ;
-    I2C1->CR1 |= (1<<8) ;    //GENERATE START
-     while (!(I2C1->SR1 & (1<<0))) ; // wait for SB bit to set
+    I2C1->CR1 |= (1 << 10) ;
+    I2C1->CR1 |= (1 << 8) ;    //GENERATE START
+     while (!(I2C1->SR1 & (1 << 0))) ; // wait for SB bit to set
 }
 
 
 void I2C_Address (uint8_t Address)
 {
     I2C1->DR = Address ; // send the address
-    while (!(I2C1->SR1 & (1<<1))) ; //wait for addr bit to set
-    uint8_t temp = I2C1->SR1 |  I2C1->SR2 ;  //read SR1 and SR2 to clear 
+    while (!(I2C1->SR1 & (1 << 1))) ; //wait for addr bit to set
+    uint8_t temp = I2C1->SR1 | I2C1->SR2 ;  //read SR1 and SR2 to clear 
 }
 
 void I2C_Stop(void)
@@ -45,15 +45,15 @@ void I2C_Stop(void)
 
 void I2C_WriteMulti (uint8_t * data, uint8_t size)
 {
-    while (!(I2C1->SR1 & (1<<7))) ; // wait for TXE bit to set
-    while(size)
+    while (!(I2C1->SR1 & (1 << 7))) ; // wait for TXE bit to set
+    while (size)
     {
-        while(!(I2C1->SR1 & (1<<7))) ;
-        I2C1->DR = (uint32_t ) *data++ ;
+        while (!(I2C1->SR1 & (1 << 7))) ;
+        I2C1->DR = (uint32_t) *data++ ;
         size-- ;
     }
 
-    while(!(I2C1->SR1 & (1<<2))) ;
+    while (!(I2C1->SR1 & (1 << 2))) ;
 }
 
 void I2C_Read (uint8_t Address, uint8_t *buffer, uint8_t size)
@@ -65,18 +65,18 @@ void I2C_Read (uint8_t Address, uint8_t *buffer, uint8_t size)
         I2C1->DR = Address ;
         while(!(I2C1->SR1 & (1 << 1))) ;
 
-        I2C1->CR1 &= ~(1<<10) ;
+        I2C1->CR1 &= ~(1 << 10) ;
         uint8_t temp = I2C1->SR1 | I2C1->SR2 ;
-        I2C1->CR1 |= (1<<9) ;
+        I2C1->CR1 |= (1 << 9) ;
 
-        while(!(I2C1->SR1 & (1<<6))) ;
+        while(!(I2C1->SR1 & (1 << 6))) ;
 
         buffer[size-remaining] = I2C1->DR ;
     }
     else
     {
         I2C1->DR =Address ;
-        while(!(I2C1->SR1 & (1<<1))) ;
+        while(!(I2C1->SR1 & (1 << 1))) ;
 
         uint8_t temp = I2C1->SR1 | I2C1->SR2 ;
 
