62f058c2ca9fdde4ac73a20cb9d704deb87e29c0
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-18 09:12:17 +0900

    Update I2C.h

--- final_project_control/master/I2C.h
+++ final_project_control/master/I2C.h
@@ -14,16 +14,16 @@
 
 
  
-void pin_setup();
-void mpu_setup();
-void mpu_write(uint8_t Address, uint8_t Reg , uint8_t Data);
-void mpu_read(uint8_t Address, uint8_t Reg , uint8_t *buffer, uint8_t size);
+void pin_setup() ;
+void mpu_setup() ;
+void mpu_write(uint8_t Address, uint8_t Reg , uint8_t Data) ;
+void mpu_read(uint8_t Address, uint8_t Reg , uint8_t *buffer, uint8_t size) ;
 
 
-void I2C_Config (void);
-void I2C_Write(uint8_t data);
-void I2C_Start (void);
-void I2C_Address (uint8_t Address);
-void I2C_Stop(void);
-void I2C_WriteMulti (uint8_t * data, uint8_t size);
-void I2C_Read (uint8_t Address, uint8_t *buffer, uint8_t size);
+void I2C_Config (void) ;
+void I2C_Write (uint8_t data) ; 
+void I2C_Start (void) ;
+void I2C_Address (uint8_t Address) ;
+void I2C_Stop (void) ;
+void I2C_WriteMulti (uint8_t * data, uint8_t size) ;
+void I2C_Read (uint8_t Address, uint8_t *buffer, uint8_t size) ;
