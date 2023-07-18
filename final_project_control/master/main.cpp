fbff0de6ed1d10b8e8e958c3a45583f2df4b08c9
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-18 09:17:56 +0900

    Update main.cpp

--- final_project_control/master/main.cpp
+++ final_project_control/master/main.cpp
@@ -119,62 +119,63 @@ int main()
     while (!hm10.readable()) {}
     
     char c = '0' ;
-    int ack_counter=0;
-    while(c != '&' && ack_counter < 1000){
-        hm10.read(&c, 1);
+    int ack_counter = 0; 
+    while(c != '&' && ack_counter < 1000)
+    {
+        hm10.read(&c, 1) ;
         if (c != '&')
         {
-            hm10.write("&", 1);
-            ack_counter += 1;
+            hm10.write("&", 1) ;
+            ack_counter += 1 ;
         }
-        else{
-            break;
+        else
+        {
+            break ;
         }
     }
-    if(c!='&'){
-        pc.write("no ACK. Reset Please\r\n", 22);
-        while(1){}
+    if(c != '&') 
+    {
+        pc.write("no ACK. Reset Please\r\n", 22) ;
+        while(1) {}
     }
 
-    pc.write("ACK received. Sending start!\r\n", 30); // received &, and can proceed with instructions
-    ThisThread::sleep_for(3000ms);
-    char cmd[21]={0x0};
-    mpu_setup();
-    //TIM6Config();
-    MPU6050_Init();
-    timer.attach(&trig_mpu_flag,70ms);
+    pc.write("ACK received. Sending start!\r\n", 30) ;  // received &, and can proceed with instructions
+    ThisThread::sleep_for(3000ms) ;
+    char cmd[21] = {0x0} ;
+    mpu_setup() ;
+    //TIM6Config() ;
+    MPU6050_Init() ;
+    timer.attach(&trig_mpu_flag, 70ms) ;
     
     while (1) // send instructions to slave to turn on LEDs
     {
-        
-        while(mpu_flag == 0)
+        while (mpu_flag == 0)
         {
 
         }
         if (hm10.writable())
         { 
-            timer.detach();     
-            uint8_t Rx_data[6];        
-            mpu_read(MPU6050_ADDR,ACCEL_XOUT_H_REG, Rx_data, 6);
-            mpu_flag=0;
+            timer.detach() ;     
+            uint8_t Rx_data[6] ;        
+            mpu_read(MPU6050_ADDR,ACCEL_XOUT_H_REG, Rx_data, 6) ;
+            mpu_flag = 0 ;
           
-            uint8_t Rxr_data[6];
-            for(int i=0; i<6;i++)
+            uint8_t Rxr_data[6] ;
+            for(int i = 0 ; i < 6 ; i++)
             {
-                Rxr_data[i] = Rx_data[i];
+                Rxr_data[i] = Rx_data[i] ;
             }
-            ThisThread::sleep_for(50ms);
-            sprintf(cmd, "%u,%u,%u,%u,%u,%u\r\n",
-            Rxr_data[0],Rxr_data[1],Rxr_data[2],Rxr_data[3],Rxr_data[4],Rxr_data[5]);
-            ThisThread::sleep_for(50ms);
-            pc.write(cmd,sizeof(cmd));
-            hm10.write(cmd, sizeof(cmd));
+            ThisThread::sleep_for(50ms) ;
+            sprintf(cmd, "%u,%u,%u,%u,%u,%u\r\n", Rxr_data[0],Rxr_data[1],Rxr_data[2],Rxr_data[3],Rxr_data[4],Rxr_data[5]);
+            ThisThread::sleep_for(50ms) ;
+            pc.write(cmd,sizeof(cmd)) ;
+            hm10.write(cmd, sizeof(cmd)) ;
             
-            timer.attach(&trig_mpu_flag,70ms);    
+            timer.attach(&trig_mpu_flag,70ms) ;    
         }
 
         
-        ThisThread::sleep_for(100ms);
+        ThisThread::sleep_for(100ms) ;
         
     }
 }
