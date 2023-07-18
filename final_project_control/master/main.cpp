3ad833d6932b019bc5f72408c47ceb0438864386
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-18 09:15:43 +0900

    Update main.cpp

--- final_project_control/master/main.cpp
+++ final_project_control/master/main.cpp
@@ -5,46 +5,47 @@
 #include "move_alpha.h"
 
 
- void MPU6050_Init();
- void mpu6050_read_accel();
-
+ void MPU6050_Init() ;
+ void mpu6050_read_accel() ;
 //--------------------------------------------------MPU VALUE------------------------------------------------------------------------
 
-
-
 //-------------------------------------------------bluetooth code and timer------------------------------------------------------------
-Ticker timer;
-BufferedSerial hm10(PA_9, PA_10, 9600); // TX, RX, baud rate 9600
-BufferedSerial pc(CONSOLE_TX, CONSOLE_RX, 9600);
+Ticker timer ;
+BufferedSerial hm10(PA_9, PA_10, 9600) ;  // TX, RX, baud rate 9600 
+BufferedSerial pc(CONSOLE_TX, CONSOLE_RX, 9600) ;
 
 
-volatile int mpu_flag=0;
-char buf[80];
+volatile int mpu_flag = 0 ;
+char buf[80] ;
 
-void trig_mpu_flag(){ 
-    mpu_flag=1;
+void trig_mpu_flag()
+{ 
+    mpu_flag = 1 ;
 }
+
 //----------------------------------------------------------------------------------------------------------------------------------
 
-void MPU6050_Init (void){
-    uint8_t check;
-    uint8_t Data;
+void MPU6050_Init (void)
+{
+    uint8_t check ;
+    uint8_t Data ;
 
-    mpu_read (MPU6050_ADDR, WHO_AM_I_REG,&check,1);
+    mpu_read (MPU6050_ADDR, WHO_AM_I_REG, &check, 1) ; 
     
-    if(check ==104){// 0X68 WILL BE RETURNED BY THE SENSOR
-        Data =0;
-        mpu_write(MPU6050_ADDR, PWR_MGMT_1_REG, Data);
+    if(check == 104)
+    {// 0X68 WILL BE RETURNED BY THE SENSOR
+        Data = 0 ;
+        mpu_write(MPU6050_ADDR, PWR_MGMT_1_REG, Data) ;
         //PWR_MGMT_1_REG
-        Data =0x07;
+        Data = 0x07 ;
 
-        mpu_write(MPU6050_ADDR, SMPLT_DIV_REG, Data);
+        mpu_write(MPU6050_ADDR, SMPLT_DIV_REG, Data) ;
         //->
         //SMPLT_DIV_REG 0x19
-        Data = 0x00;
-        mpu_write(MPU6050_ADDR, ACCEL_CONFIG_REG, Data);
+        Data = 0x00 ;
+        mpu_write(MPU6050_ADDR, ACCEL_CONFIG_REG, Data) ;
         //ACCEL_CONFIG_REG 0x1C
-        Data = 0x00;
+        Data = 0x00 ;
 
         mpu_write(MPU6050_ADDR, GYRO_CONFIG_REG, Data);
         //GYRO_CONFIG_REG 0x1B
@@ -58,22 +59,26 @@ void SetingDataReceived()
 {
     while (hm10.readable()) // flush hm10 buffer
     {
-        char raw[80];
-        char buf[80];
+        char raw[80] ;
+        char buf[80] ;
         
-        hm10.read(raw, sizeof(raw));
-        if (raw[0]=='O'){
-            for (int i = 0; i < sizeof(raw); i++){
-                if (raw[i]=='\r' || raw[i]=='\n' || raw[i]=='\0'){
-                    buf[i]='\r';
-                    buf[i+1]='\n';
-                    break;
+        hm10.read(raw, sizeof(raw)) ; 
+        if (raw[0] == 'O')
+        {
+            for (int i = 0 ; i < sizeof(raw) ; i++)
+            {
+                if (raw[i] == '\r' || raw[i] == '\n' || raw[i] == '\0')
+                {
+                    buf[i] = '\r' ;
+                    buf[i + 1] = '\n' ;
+                    break ;
                 }
-                else{
-                    buf[i]=raw[i];
+                else
+                {
+                    buf[i] = raw[i] ;
                 }
             }
-            pc.write(buf, sizeof(buf));
+            pc.write(buf, sizeof(buf)) ;
         }
     }
     ThisThread::sleep_for(50ms);
@@ -83,38 +88,37 @@ void SetingDataReceived()
 
 int main()
 {   
-
     while (!hm10.writable()) {} // wait until writeable
-    ThisThread::sleep_for(2000ms);
-    pc.write("\r\nStarting Master Program\r\n", 27);
-    hm10.write("AT+RENEW\r\n", 10);
-    pc.write("Device Reset\r\n", 14);
-    ThisThread::sleep_for(3000ms);
-    SetingDataReceived();
+    ThisThread::sleep_for(2000ms) ;
+    pc.write("\r\nStarting Master Program\r\n", 27) ;
+    hm10.write("AT+RENEW\r\n", 10) ;
+    pc.write("Device Reset\r\n", 14) ;
+    ThisThread::sleep_for(3000ms) ;
+    SetingDataReceived() ;
 
-    hm10.write("AT+ROLE1\r\n", 10); // set chip to master mode
-    ThisThread::sleep_for(3000ms);
-    SetingDataReceived();
+    hm10.write("AT+ROLE1\r\n", 10) ; // set chip to master mode
+    ThisThread::sleep_for(3000ms) ;
+    SetingDataReceived() ;
 
-    hm10.write("AT+NAMEKDH\r\n", 13);
-    pc.write("Device Set to Master\r\n", 22);
-    ThisThread::sleep_for(3000ms);
-    SetingDataReceived();
+    hm10.write("AT+NAMEKDH\r\n", 13) ;
+    pc.write("Device Set to Master\r\n", 22) ;
+    ThisThread::sleep_for(3000ms) ;
+    SetingDataReceived() ;
 
 
-    pc.write("Connecting to device YS\r\n", 27);
-    hm10.write("AT+CONN=YS\r\n", 14); // connects to the first device in the list
-    ThisThread::sleep_for(5000ms); // wait to connect
-    SetingDataReceived();
+    pc.write("Connecting to device YS\r\n", 27) ;
+    hm10.write("AT+CONN=YS\r\n", 14) ; // connects to the first device in the list
+    ThisThread::sleep_for(5000ms) ; // wait to connect
+    SetingDataReceived() ;
 
-    while(!hm10.writable()){}
-    hm10.write("&", 1); // send & to test if the connection is established
-    pc.write("Sending test character, waiting for ACK\r\n", 41);
-    ThisThread::sleep_for(2000ms);
+    while(!hm10.writable()) {}
+    hm10.write("&", 1) ; // send & to test if the connection is established
+    pc.write("Sending test character, waiting for ACK\r\n", 41) ;
+    ThisThread::sleep_for(2000ms) ;
 
-    while (!hm10.readable()){}
+    while (!hm10.readable()) {}
     
-    char c='0';
+    char c = '0' ;
     int ack_counter=0;
     while(c != '&' && ack_counter < 1000){
         hm10.read(&c, 1);
