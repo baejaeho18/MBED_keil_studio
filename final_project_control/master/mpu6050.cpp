72ae24c2d4edde2b0a64acb702633e2a565a9dc0
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-24 09:39:07 +0900

    Update mpu6050.cpp

--- final_project_control/master/mpu6050.cpp
+++ final_project_control/master/mpu6050.cpp
@@ -2,55 +2,59 @@
 #include "I2C.h"
 #include "mpu6050.h"
 
-void pin_setup(){
+void pin_setup()
+{
     //I2C clock
-    __HAL_RCC_I2C1_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_I2C1_CLK_ENABLE() ;
+    __HAL_RCC_GPIOB_CLK_ENABLE() ;
     //MODER-> PB6: ALTERNATIVE FUNCTIONMODE
-    GPIOB->MODER &= ~(0b11<<12);
-    GPIOB->MODER |= (0b10<<12);
+    GPIOB->MODER &= ~(0b11<<12) ;
+    GPIOB->MODER |= (0b10<<12) ;
     //MODER-> PB7: ALTERNATIVE FUNCTIONMODE
-    GPIOB->MODER &= ~(0b11<<14);
-    GPIOB->MODER |= (0b10<<14);
+    GPIOB->MODER &= ~(0b11<<14) ;
+    GPIOB->MODER |= (0b10<<14) ;
     //set GPIO PB6,PB7 port to open-drain
-    GPIOB->OTYPER |= (0b11<<6);
+    GPIOB->OTYPER |= (0b11<<6) ;
     // pull-up timp->scl, sda default value is high for i2c communication
-    GPIOB->PUPDR &= ~(0b11<<12);
-    GPIOB->PUPDR |= (0b01<<12);
+    GPIOB->PUPDR &= ~(0b11<<12) ;
+    GPIOB->PUPDR |= (0b01<<12) ;
     //set pull-up pb7
-    GPIOB->PUPDR &= ~(0b11<<14);
-    GPIOB->PUPDR |= (0b01<<14);
+    GPIOB->PUPDR &= ~(0b11<<14) ;
+    GPIOB->PUPDR |= (0b01<<14) ;
     //HIGH SPEED
-    GPIOB->OSPEEDR |= (3 << 12) |( 3 << 14);
+    GPIOB->OSPEEDR |= (3 << 12) |( 3 << 14) ;
     //i2c scl->PB6->D10
-    GPIOB->AFR[0] &= ~(0B1111<<24);
-    GPIOB->AFR[0] |= (0B0100<<24);
+    GPIOB->AFR[0] &= ~(0B1111<<24) ;
+    GPIOB->AFR[0] |= (0B0100<<24) ;
     //i2c sda->PB7->GND
-    GPIOB->AFR[0] &= ~(0B1111<<28);
-    GPIOB->AFR[0] |= (0B0100<<28);
+    GPIOB->AFR[0] &= ~(0B1111<<28) ; 
+    GPIOB->AFR[0] |= (0B0100<<28) ;
 }
 
-void mpu_setup(){
-    pin_setup();
-    I2C_Config();
+void mpu_setup()
+{
+    pin_setup() ;
+    I2C_Config() ;
 }
 
-void mpu_write(uint8_t Address, uint8_t Reg , uint8_t Data){
-    I2C_Start();
-    I2C_Address(Address);
-    I2C_Write (Reg);
-    I2C_Write (Data);
-    I2C_Stop();
+void mpu_write(uint8_t Address, uint8_t Reg , uint8_t Data)
+{
+    I2C_Start() ;
+    I2C_Address(Address) ;
+    I2C_Write (Reg) ;
+    I2C_Write (Data) ;
+    I2C_Stop() ;
 }
 
 
 
-void mpu_read(uint8_t Address, uint8_t Reg , uint8_t *buffer, uint8_t size){
-    I2C_Start();
-    I2C_Address (Address);
-    I2C_Write (Reg);
-    I2C_Start (); //REPEATSTART
-    I2C_Read (Address+0x01,buffer,size);
-    I2C_Stop ();
-    ThisThread::sleep_for(50ms);
+void mpu_read(uint8_t Address, uint8_t Reg , uint8_t *buffer, uint8_t size)
+{
+    I2C_Start() ;
+    I2C_Address (Address) ; 
+    I2C_Write (Reg) ;
+    I2C_Start () ; //REPEATSTART
+    I2C_Read (Address+0x01,buffer,size) ;
+    I2C_Stop () ;
+    ThisThread::sleep_for(50ms) ;
 }
