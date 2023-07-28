4086c280d82726a8baa0b8711eaadec38bc9dc93
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-07-28 11:30:22 +0900

    Update move_alpha.cpp

--- final_project_control/slave/move_alpha.cpp
+++ final_project_control/slave/move_alpha.cpp
@@ -10,43 +10,48 @@
 #define accel 0.1
 #define rotate_accel 0.8
 
-DigitalOut motorLeftPin1(A1);    // 왼쪽 모터 회전 방향 핀 1
-DigitalOut motorLeftPin2(A0);    // 왼쪽 모터 회전 방향 핀 2
-DigitalOut motorRightPin1(A2);   // 오른쪽 모터 회전 방향 핀 1
-DigitalOut motorRightPin2(A3);  // 오른쪽 모터 회전 방향 핀 2
-PwmOut motorLeftPWMPin(D6);      // 왼쪽 모터 속도 제어 핀PA 
-PwmOut motorRightPWMPin(D5);     // 오른쪽 모터 속도 제어 핀 PB
+DigitalOut motorLeftPin1(A1) ;    // 왼쪽 모터 회전 방향 핀 1
+DigitalOut motorLeftPin2(A0) ;    // 왼쪽 모터 회전 방향 핀 2
+DigitalOut motorRightPin1(A2) ;   // 오른쪽 모터 회전 방향 핀 1
+DigitalOut motorRightPin2(A3) ;  // 오른쪽 모터 회전 방향 핀 2
+PwmOut motorLeftPWMPin(D6) ;      // 왼쪽 모터 속도 제어 핀PA 
+PwmOut motorRightPWMPin(D5) ;     // 오른쪽 모터 속도 제어 핀 PB
 //pwm 핀은 변경불가능하고  나머지 핀을 사용할 경우 다른 핀으로 변경만 해주면 됩니다. 
 
-void moveAlpha(float stack, int direction){
-    float speed = accel * (stack+1) * weight;
+void moveAlpha(float stack, int direction)
+{
+    float speed = accel * (stack + 1) * weight ;
 
-    if (direction == FORWARD){
-        motorLeftPin1 = 0;
-        motorLeftPin2 = 1;
-        motorRightPin1 = 0;
-        motorRightPin2 = 1;
+    if (direction == FORWARD)
+    {
+        motorLeftPin1 = 0 ;
+        motorLeftPin2 = 1 ;
+        motorRightPin1 = 0 ;
+        motorRightPin2 = 1 ;
 
-        motorLeftPWMPin.write(speed);
-        motorRightPWMPin.write(speed);
+        motorLeftPWMPin.write(speed) ;
+        motorRightPWMPin.write(speed) ; 
     }
-    else if (direction == BACKWARD) {
-        motorLeftPin1 = 1;
-        motorLeftPin2 = 0;
-        motorRightPin1 = 1;
-        motorRightPin2 = 0;
-        motorLeftPWMPin.write(speed);
-        motorRightPWMPin.write(speed);
+    else if (direction == BACKWARD) 
+    {
+        motorLeftPin1 = 1 ;
+        motorLeftPin2 = 0 ;
+        motorRightPin1 = 1 ;
+        motorRightPin2 = 0 ;
+        motorLeftPWMPin.write(speed) ;
+        motorRightPWMPin.write(speed) ;
     }
-    else if (direction == LEFT) {
-        motorLeftPin1 = 1;
-        motorLeftPin2 = 0;
-        motorRightPin1 = 0;
-        motorRightPin2 = 1;
-        motorLeftPWMPin.write(speed*rotate_accel);
-        motorRightPWMPin.write(speed*rotate_accel);
+    else if (direction == LEFT) 
+    {
+        motorLeftPin1 = 1 ;
+        motorLeftPin2 = 0 ;
+        motorRightPin1 = 0 ;
+        motorRightPin2 = 1 ;
+        motorLeftPWMPin.write(speed * rotate_accel) ;
+        motorRightPWMPin.write(speed * rotate_accel) ;
     }
-    else if (direction == RIGHT) {
+    else if (direction == RIGHT) 
+    {
         motorLeftPin1 = 0;  
         motorLeftPin2 = 1;
         motorRightPin1 = 1;
