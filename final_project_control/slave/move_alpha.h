53303c263dcefa71b286bae611b15bb47495e295
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-08-02 16:23:59 +0900

    Update move_alpha.h

--- final_project_control/slave/move_alpha.h
+++ final_project_control/slave/move_alpha.h
@@ -1,26 +1,20 @@
 #include "mbed.h"
 
-
-
-
 // 초기 설정
 void setup() ;
-
-void moveAlpha(float stack, int direction);
+void moveAlpha(float stack, int direction) ;
 
 // // 앞으로 이동
 // void moveForward(float stack) ;
 
 // // 뒤로 이동
-// void moveBackward(float stack);
-
+// void moveBackward(float stack) ;
 
 // // 좌회전
-// void turnLeft(float stack);
-
+// void turnLeft(float stack) ;
 
 // // 우회전
 // void turnRight(float stack) ;
 
 // // 정지
-// void stopMoving(float stack);
+// void stopMoving(float stack) ;
